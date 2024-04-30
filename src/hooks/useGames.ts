fd6d09e270c3cc5a61382ef1e4907ef2068e55ab
IMPIGERTECH\naveenkumar.magesh <naveenkumar.magesh@impigertech.com>
2024-04-30 15:09:20 +0530

    show loading skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -28,15 +28,25 @@ const useGames = () => {
 
     const [error,setError]=useState('');
 
+    const [isLoading,setLoading] = useState(false);
+
     useEffect(()=> {
 
         const controller = new AbortController();
 
+        setLoading(true);
+
         apiClient.get<FetchGamesResponse>('/games',{signal: controller.signal})
-        .then(res=>setGames(res.data.results))
+        .then((res)=>
+         {           
+         setGames(res.data.results);
+         setLoading(false);
+
+         }    )
         .catch(err=>{
             if(err instanceof CanceledError) return;
             setError(err.message)
+            setLoading(false)
         }
         );
 
@@ -44,7 +54,7 @@ const useGames = () => {
 
     },[]);
 
-    return {games,error};
+    return {games,error,isLoading};
 
 }
 
