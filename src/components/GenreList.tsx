790f91848851e25daf16452335b9fac55248b84b
IMPIGERTECH\naveenkumar.magesh <naveenkumar.magesh@impigertech.com>
2024-05-01 00:06:59 +0530

    creating an genric fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import useGenres from '../hooks/useGenre'
+import useGenres, { Genre } from '../hooks/useGenre'
+import useData from '../hooks/useData';
 
 const GenreList = () => {
 
-    const {genres}= useGenres();
+    const {data}= useGenres();
 
   return (
     <ul>
-        {genres.map(genre=><li key={genre.id}>
+        {data.map(genre=><li key={genre.id}>
             {genre.name}
         </li>)}
     </ul>
